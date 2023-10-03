c699c7ee3963d54faa2722d7127dcddce3339bab
pablodamianr1983 <damianpabloroldan@gmail.com>
2023-10-02 21:26:48 -0300

    update

--- app/templates/front_pinpals-master/static/js/animation.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    setTimeout(function() {
-        document.querySelector('.container').classList.add('visible');
-    }, 2100); 
-});
-
